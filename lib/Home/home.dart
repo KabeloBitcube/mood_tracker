aeb2cd6ff7ba384e0fb4f664f32c3b017a70ad21
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-05-30 11:52:52 +0200

    V2 - Home screen body > Welcome text amd Mood carousel added

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -15,17 +16,64 @@ class HomeScreen extends StatelessWidget {
           },
           icon: Icon(Icons.dark_mode_outlined),
         ),
-        title: Text(getCurrentDate(), style: TextStyle(fontSize: 15),),
+        title: Text(getCurrentDate(), style: TextStyle(fontSize: 15)),
         actions: [
           Padding(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.only(right: 16),
             child: Icon(Icons.calendar_month_outlined),
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Center(child: Text('Home!'))],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              'How Are You Feeling Today?',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                wordSpacing: 5,
+              ),
+            ),
+            const SizedBox(height: 10),
+            CarouselSlider(
+              options: CarouselOptions(height: 250.0),
+              items: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(250),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(250),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(250),
+                  ),
+                  child: Center(
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(250),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }

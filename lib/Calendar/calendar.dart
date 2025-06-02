cdf3f600792ffa53882ca290c2f8c58b506d894b
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-06-02 11:05:37 +0200

    V11 - Calendar screen > Added a mood entry details container

--- lib/Calendar/calendar.dart
+++ lib/Calendar/calendar.dart
@@ -9,7 +9,6 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +22,160 @@ class _CalendarScreenState extends State<CalendarScreen> {
             headerStyle: HeaderStyle(
               titleCentered: true,
               formatButtonVisible: false,
+            ),
+          ),
+          Container(
+            height: 494.2,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(100),
+                topRight: Radius.circular(100),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                      right: 150,
+                      left: 150,
+                    ),
+                    child: Divider(thickness: 2),
+                  ),
+                  Text(
+                    'Tuesday, 2 June',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 30,
+                      right: 45,
+                      top: 10,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Morning', style: TextStyle(color: Colors.white)),
+                        Text(
+                          'Afternoon',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text('Night', style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlue,
+                            borderRadius: BorderRadius.circular(200),
+                          ),
+                          child: Center(
+                            child: Image.asset('assets/images/Sad.webp'),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(200),
+                          ),
+                          child: Center(
+                            child: Image.asset('assets/images/Happy.webp'),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(200),
+                          ),
+                          child: Center(
+                            child: Image.asset('assets/images/Angry.png'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Divider(thickness: 1),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Reason',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(fontSize: 16),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      contentPadding: EdgeInsets.all(16),
+                      fillColor: Colors.black54,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Description',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(fontSize: 16),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      contentPadding: EdgeInsets.all(16),
+                      fillColor: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],

ab2a47b2e90a33db472cdabc83f339d875f6884b
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-06-02 10:10:08 +0200

    V10 - Calendar screen > Added calendar on calendar screen and navigation

--- lib/Calendar/calendar.dart
+++ lib/Calendar/calendar.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget{
-  const Calendar({super.key});
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: [],
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: DateTime.now(),
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false,
+            ),
+          ),
+        ],
       ),
     );
   }

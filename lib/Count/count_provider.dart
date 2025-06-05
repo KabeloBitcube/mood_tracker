bfd36f52798b0449ac9ea5f3f27c8a76c7205f3e
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-06-05 16:50:05 +0200

    V48 - Final version

--- lib/Count/count_provider.dart
+++ lib/Count/count_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CountProvider extends ChangeNotifier{
+  //Tracks the notification count
   int count = 0;
 
+
+  
   void getNotificationCount (int currentCount){
     count = currentCount;
     notifyListeners();

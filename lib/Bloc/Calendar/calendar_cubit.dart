9f160e595e06d61db6cfb97273b25ef231fe7bf1
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-06-12 16:28:12 +0200

    V66 - Bloc integration > Calendar cubit updates mood entry on calendar screen

--- lib/Bloc/Calendar/calendar_cubit.dart
+++ lib/Bloc/Calendar/calendar_cubit.dart
@@ -1,10 +1,19 @@
 import 'package:bloc/bloc.dart';
-import 'package:mood_tracker/Calendar/calendar.dart';
+import 'package:mood_tracker/Mood_model/moodentry.dart';
 
-class CalendarCubit extends Cubit<CalendarScreen>{
-  CalendarCubit() : super(CalendarScreen(moodEntries: []));
+class CalendarCubit extends Cubit<MoodEntry> {
+  CalendarCubit()
+    : super(
+        MoodEntry(
+          mood: '',
+          reason: '',
+          description: '',
+          timeOfDay: 0,
+          date: DateTime.now(),
+        ),
+      );
 
-  void update (){
-    emit(state);
+  void update (MoodEntry entry) {
+    emit(entry);
   }
 }
